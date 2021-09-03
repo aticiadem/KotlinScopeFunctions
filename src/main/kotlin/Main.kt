c36849fc6ddf2a466cd0b0ff1bd7586163d73c59
adematici <adematicis41@gmail.com>
2021-09-03 10:23:16 +0300

    with scope function added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
-    letScopeFunction()
+    //letScopeFunction()
+    withScopeFunction()
 }
 
 private fun letScopeFunction() {
@@ -10,5 +11,13 @@ private fun letScopeFunction() {
     str = "Adem"
     str?.let {
         println("Hello $it")
+    }
+}
+
+private fun withScopeFunction() {
+    val numbers = arrayListOf<Int>(1, 2, 3)
+    with(numbers) {
+        println("We have $size numbers")
+        println("Last number is ${last()}")
     }
 }
